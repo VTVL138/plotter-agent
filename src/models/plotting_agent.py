8dafbd5f5d4de3bd83aa999c765ecd34d11d2d72
VTVL138 <VTVL138>
2025-05-07 21:49:30 +0200

    added postprocess to python generator

--- src/models/plotting_agent.py
+++ src/models/plotting_agent.py
@@ -4,18 +4,39 @@ from pydantic import BaseModel
 
 from models import config
 import re
+import logging
+
+logger = logging.getLogger(__name__)
 class ChatWithOllama(BaseModel):
     model_name: str = config["coding_model_name"]
     system_instructions: str = config["coding_model_instruction"]
 
-    def call_LLM(self,prompt:str):
+
+    def call_LLM(self,prompt:str) -> str:
         response: ChatResponse = chat(model='codellama',
                                       messages=[{'role': 'system','content': self.system_instructions},
                                                 {'role': 'user','content': prompt}])
-        code_str = ChatWithOllama.clean_python_LLM_response(response["message"]["content"])
-        return code_str
+        return response["message"]["content"]
+
+
+    def call_LLM_python(self,prompt:str) -> str:
+        response = self.call_LLM(prompt=prompt)
+        print("Response:",response)
+        try:
+            code_str = ChatWithOllama.clean_python_LLM_response(text=response)
+            return code_str
+        
+        except BaseException as e:
+            logger.warning(f"Unexpected error while parsing generated python code string, error msg: {repr(e)}")
+            return None
+        
     
     @staticmethod
     def clean_python_LLM_response(text:str):
-        return re.search(r"```python([\s\S]*?)\n```",text).group(1)
+        if text.startswith("```python") and text.endswith("```"):
+            return re.search(r"```python([\s\S]*?)```",text).group(1)
+        elif text.startswith("```") and text.endswith("```"):
+            return re.search(r"```([\s\S]*?)```",text).group(1)
+        else:
+            return text
     
